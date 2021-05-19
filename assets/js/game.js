4b9871d733af862f737f9646e7ed2ab73f49f076
Derek Beckford <dbeck1207@icloud.com>
2021-05-19 18:42:51 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,14 +3,18 @@
 //    * Fight all enemy-robots
 //    * Defeat each enemy-robot
 // "LOSE" - Player robot's health is zero or less
-
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+  return value;
+};
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
-var enemyHealth = 50;
+var enemyHealth = randomNumber(40, 60);
+console.log(enemyHealth)
 var enemyAttack = 12;
 
 var fight = function(enemyName){
@@ -34,7 +38,8 @@ var fight = function(enemyName){
     }
 
     // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+    enemyHealth = Math.max(0, enemyHealth - damage);
     console.log(
         playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
     );
@@ -53,7 +58,8 @@ var fight = function(enemyName){
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+    playerHealth = Math.max(0, playerHealth - damage);
     console.log(
         enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
@@ -80,6 +86,7 @@ var fight = function(enemyName){
           window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ));
           // pick new enemy to fight based on the index of the enemyNames array
           var pickedEnemyName = enemyNames[i];
+          console.log(enemyHealth)
 
           // reset enemyHealth before starting new fight
           enemyHealth = 50;
